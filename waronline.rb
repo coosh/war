6b0b8550ea0b097a19072f4d4d7b93c99cabfbcb
Robert Postill <rpostill@gmail.com>
2010-04-19 17:24:05 +0000

    modified:   waronline.rb

--- waronline.rb
+++ waronline.rb
@@ -1,112 +1,82 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
+# Warhammer parsing script
 #
-## Runtime Variables
+gid = "1226"
+sid = "2"
 
-guildid=1226
-serverid=2
-
-$mysqlhost="localhost"
-$mysqlusername="XX"
-$mysqlpassword=""
-$mysqlDB="" 
-
-## Static Variables
-$chars =  {}
-$charcount=0
-$debug=3
-
-## Required modules
+# requirements
 require 'net/http'
-require 'mysql'
+require 'rubygems'
+require 'hpricot'
+require 'active_record'
+chars = {}
+ActiveRecord::Base.logger = Logger.new(STDERR)
 
-## Objects
-class Character
-	def initialize(cname, level, id, gid, sid, indb, status)
-		@cname = cname
-		@level = level
-		@id = id
-		@gid = gid
-		@sid = sid
-		@indb = indb
-		@status = status
-	end
-	def to_s
-		"Character: #@cname, #@level\n"
-	end
-	attr_writer :rr, :id, :chcl, :exp, :rrexp, :sid, :gid, :indb
-	attr_reader :cname, :level, :rr, :id, :chcl, :exp, :rrexp, :sid, :gid, :status, :indb, :sid
+ActiveRecord::Base.establish_connection(
+  :adapter => 'mysql',
+  :host => 'localhost',
+  :username => '',
+  :password => '',
+  :database => ''
+)
+class Character < ActiveRecord::Base
+ 
+end
+class Charparse
+  attr_accessor :name, :level, :idtag, :status, :career
+  def initialize(name, level, status, career, idtag)
+    @name = name
+    @level = level
+    @status = status
+    @career = career
+    @idtag = idtag
+  end
 end
 
-## Functions
-def strip_html(str)
-	str = str.strip || ''
-	str.gsub(/<\/?[^>]*>/, '')
+chars = {}
+unless Character.table_exists?
+  ActiveRecord::Schema.define do
+    create_table :Characters do |table|
+      table.column :idtag, :string
+      table.column :name, :string
+      table.column :career, :string
+      table.column :level, :string
+      table.column :exp, :string
+      table.column :rr, :string
+      table.column :rrexp, :string
+      table.column :status, :string
+      table.column :gid, :string
+      table.column :sid, :string
+      table.column :timestamp, :timestamp
+    end
+  end
 end
 
-def load_warstatus(gid, sid)
-	gid=gid.to_s
-	sid=sid.to_s
-	searchurl = "https://realmwar.warhammeronline.com/realmwar/GuildInfo.war?id=#{gid}&server=#{sid}"
-	Net::HTTP.get(URI.parse(searchurl))
+# Load guild webpage status and update DB
+searchurl = "https://realmwar.warhammeronline.com/realmwar/GuildInfo.war?id=#{gid.to_s}&server=#{sid.to_s}"
+guildpage = Hpricot.parse(Net::HTTP.get(URI.parse(searchurl)))
+guildpage.search(" //div/table[@summary='Guild Roster']/tbody ").each_with_index do |line, index|
+  charsearch = Hpricot.parse(line.to_s)
+  status = (charsearch/:img).first[:title]
+  status = status.match(/O[fn]{1,2}line/)
+  name = (charsearch/:a).inner_html
+  idtag = (charsearch/:a).first[:href]
+  idtag = idtag.match(/\d{3,10}/)
+  career = (charsearch/:img).last[:title]
+  level = (charsearch/:td).last.inner_html
+  chars[index] = Charparse.new(name.to_s, level.to_s, status.to_s, career.to_s, idtag.to_s)
+  if Character.exists?(:idtag => idtag.to_s)
+    Character.update_all("status='#{status.to_s}', level='#{level.to_s}'", :idtag => idtag.to_s)
+  else
+    Character.create(:idtag => idtag.to_s, :name => name.to_s, :career => career.to_s, :level => level.to_s, :status => status.to_s)
+  end
 end
 
-def load_list(parse, gid, sid)
-	gid=gid
-	sid=sid
-	onlinename="blank"
-	status=""
-	found="none"
-	count="blank"
-	level="blank"
-	id="0"
-	parse.each do |line|
-	        if count == "2"
-        	        level = strip_html(line)
-			indb="no"
-			$chars[$charcount] = Character.new(onlinename, level, id, gid, sid, indb, status)
-                	count = "0"
-        	end
-        	if count == "1"
-                	count = "2"
-        	end
-        	if found == "one"
-			id = line.match(/\d{3,10}/)
-			onlinename=strip_html(line)
-                	count="1"
-			found="none"
-        	end
-        	if /player-status_online/ =~ line
-                	found="one"
-			status="online"
-                	$charcount = $charcount + 1
-        	end
-
-	end
-end
-
-def insert_chars
-	y=1
-	my = Mysql.real_connect($mysqlhost, $mysqlusername, $mysqlpassword, $mysqlDB)
-	$chars.each do
-		id=$chars[y].id
-		status=$chars[y].status
-		query="SELECT * FROM onlinechars WHERE id LIKE #{id.to_s}"
-		res = my.query(query)
-		res.each do |row|
-			if row[7] != status
-				print row[7]
-				queryx="UPDATE onlinechars SET status='#{status.to_s}' WHERE id='#{id.to_s}';"
-				empty = my.query(queryx)
-			end
-		end
-		y=y+1
-	end
-	my.close
-end
-
-## Main
-begin
-	warprint = load_warstatus(guildid, serverid)
-	load_list(warprint, guildid, serverid)
-	insert_chars
+# Clean up those not in the guild anymore
+Character.all.each do |line|
+  delete = 1
+  chars.each_with_index do |charsearch, index|
+    delete = 0 if chars[index].idtag  == line.idtag
+  end
+  Character.delete_all("idtag = #{line.idtag}") if delete == 1
 end
